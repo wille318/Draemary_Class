8428fa32fa512563ba696864f74c2e46794e9352
wille318@naver.com <wille318@naver.com>
2020-07-11 17:48:08 +0900

    5강 템테 에러있음

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('introduce/', views.introduce, name = "introduce"),
+    path('profile/<int:designer_id>', views.detail, name="detail"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- page/models.py
+++ page/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Designer(models.Model) : 
+    objects = models.Manager()
     image = models.ImageField(upload_to="images/")
     name = models.CharField(max_length = 50)
     address = models.CharField(max_length = 255)
